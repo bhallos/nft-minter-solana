22aecd6bb7005044fda8c62296bf3926c4fbd311
NFT MINTERFRONTEND <himanshuthakurmarpa@gmail.com>
2023-05-25 21:25:12 +0530

    2nd commit

--- nft-minter-solana/app/index.tsx
+++ nft-minter-solana/app/index.tsx
@@ -1,17 +1,16 @@
-import Disconnected from './components/Disconnected'
 import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
 import type { NextPage } from "next"
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import NavBar from "./components/NavBar"
-import WalletContextProvider from '../components/WalletContextProvider'
-import { useWallet } from "@solana/wallet-adapter-react"
-import Connected from "../components/Connected"
+import NavBar from "@/components/NavBar"
+import Disconnected from '@/components/Disconnected'
+
 
 const Home: NextPage = () => {
-    const { connected } = useWallet()
+
 
     return (
+
         <div className={styles.container}>
             <Head>
                 <title>Buildoors</title>
@@ -22,93 +21,33 @@ const Home: NextPage = () => {
             <Box
                 w="full"
                 h="calc(100vh)"
-                bgImage={connected ? "" : "url(/home-background.svg)"}
+                bgImage={"url(/home-background.svg)"}
                 backgroundPosition="center"
             >
-                <ChakraProvider theme={theme}>
-                    <WalletContextProvider>
-                        <Component {...pageProps} />
-                    </WalletContextProvider>
-                </ChakraProvider>
                 <Stack w="full" h="calc(100vh)" justify="center">
+                    { /* NavBar */}
                     <NavBar />
-
                     <Spacer />
-                    <Center>{connected ? <Connected /> : <Disconnected />}</Center>
+                    <Center>
+                        <Disconnected />
+                    </Center>
                     <Spacer />
 
-                    <div className={styles.container}>
-                        <Head>
-
-                            <Box
-                                w="full"
-                                h="calc(100vh)"
-                                bgImage={"url(/home-background.svg)"}
-                                backgroundPosition="center"
+                    <Center>
+                        <Box marginBottom={4} color="white">
+                            <a
+                                href="https://twitter.com/_buildspace"
+                                target="_blank"
+                                rel="noopener noreferrer"
                             >
-                                <Stack w="full" h="calc(100vh)" justify="center">
-                                    { /* NavBar */}
-                                    <NavBar />
-
-                                    <Spacer />
-                                    <Center>
-                                        <Disconnected />
-                                    </Center>
-                                    <Spacer />
-
-                                    <div className={styles.container}>
-                                        <Head>
-
-                                            <Box
-                                                w="full"
-                                                h="calc(100vh)"
-                                                bgImage={"url(/home-background.svg)"}
-                                                backgroundPosition="center"
-                                            >
-                                                <Stack w="full" h="calc(100vh)" justify="center">
-                                                    { /* NavBar */}
-                                                    <NavBar />
-
-                                                    <div className={styles.container}>
-                                                        <Head>
-                                                            <title>Buildoors</title>
-                                                            <meta name="The NFT Collection for Buildoors" />
-                                                            <link rel="icon" href="/favicon.ico" />
-                                                        </Head>
-
-                                                        <Box
-                                                            w="full"
-                                                            h="calc(100vh)"
-                                                            bgImage={"url(/home-background.svg)"}
-                                                            backgroundPosition="center"
-                                                        >
-                                                            <Stack w="full" h="calc(100vh)" justify="center">
-                                                                { /* NavBar */}
-
-                                                                <Spacer />
-                                                                <Center>
-                                                                    { /* If connected, the second view, otherwise the first */}
-                                                                </Center>
-                                                                <Spacer />
-
-                                                                <Center>
-                                                                    <Box marginBottom={4} color="white">
-                                                                        <a
-                                                                            href="https://twitter.com/_buildspace"
-                                                                            target="_blank"
-                                                                            rel="noopener noreferrer"
-                                                                        >
-                                                                            built with @_buildspace
-                                                                        </a>
-                                                                    </Box>
-                                                                </Center>
-                                                            </Stack>
-                                                        </Box>
-                                                    </div>
-                                                    )
-                                        
+                                built with @_buildspace
+                            </a>
+                        </Box>
+                    </Center>
+                </Stack>
+            </Box>
+        </div>
+    )
 }
 
-
-
-                                                    export default Home
+export default Home
